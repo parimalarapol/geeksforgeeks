9a7b5e21adabe90f5b495735565ed8ec9e19b088
Parimala Rapol <parimalajava1@gmail.com>
2017-10-05 19:10:35 -0400

    Create Stack.java

---
+++ stacks/Stack.java
@@ -0,0 +1,41 @@
+package stack;
+
+public class Stack {
+
+	int stack[];
+	int top, size, min, max;
+	
+	public Stack(int size) {
+		this.size = size;
+		stack = new int[size];
+		top = -1;
+		min = 1000;
+		max = -1;
+	}
+	
+	void push(int number) {
+		if(top == size-1) 	
+			throw new StackOverflowException();
+		else 
+			stack[++top] = number;
+	}
+	
+	int peek() {
+		return (top > -1) ? stack[top] : -1;
+	}
+	
+	void pop() {
+		if(top == -1) 
+			throw new StackUnderflowException();
+		else 
+			top--;
+	}
+	
+	boolean isEmpty() {
+		return (top == -1) ? true : false;
+	}
+	
+	boolean isFull() {
+		return (top == size-1) ? true : false;
+	}
+}
