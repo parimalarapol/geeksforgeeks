614a44d58ed01adc5a93ee79b3340002361a0be0
Parimala Rapol <parimalajava1@gmail.com>
2017-10-06 12:44:58 -0400

    Delete StackOverflowException.java

--- stacks/StackOverflowException.java
+++
@@ -1,8 +0,0 @@
-package stack;
-
-public class StackOverflowException extends Error{
-
-	public StackOverflowException() {
-		System.out.println("Cannot insert. Stack has exceeded it's limit.");
-	}
-}
